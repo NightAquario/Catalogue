e9cc7ada35a916794e43df0c66a881e6fc571d55
Baratashvili <student.nikoloz.baratashvili@gmail.com>
2024-12-07 19:36:58 +0400

    Add project files.

---
+++ Catalogue/Categories.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalogue.DTO;
+
+public class Categories
+{
+    public int Id { get; set; }
+    public required string Code { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+    public required bool IsActive { get; set; }
+    public ICollection<Products>? Products { get; set; }
+}
---
+++ Catalogue/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalogue.DTO;
+
+public class Products
+{
+    public int Id { get; set; }
+    public required string Code { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime LastUpdateDate { get; set; }
+    public required bool IsActive { get; set; }
+
+}
